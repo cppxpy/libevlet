c4df7d1af56b65936ee41f14b32ac6585e4d00dc
Sean Champ <spchamp@users.noreply.github.com>
2025-06-11 14:35:59 -0700

    scope_source : cleanups for idefs

--- source/evtlet/util/scope_source.hpp
+++ source/evtlet/util/scope_source.hpp
@@ -2,10 +2,18 @@
 
 #ifdef BEMAN_SCOPE
 #include <beman/scope/scope.hpp>
+#ifndef SCOPE_EXIT
 #define SCOPE_EXIT beman::scope::scope_exit
+#endif
+#ifndef SCOPE_FAIL
 #define SCOPE_FAIL beman::scope::scope_fail
+#endif
 #else
 #include <experimental/scope>
+#ifndef SCOPE_EXIT
 #define SCOPE_EXIT std::experimental::scope_exit
-#define SCOPE_EXIT std::experimental::scope_fail
+#endif
+#ifndef SCOPE_FAIL
+#define SCOPE_FAIL std::experimental::scope_fail
+#endif
 #endif
